aa8fedc7bb2c16cefc5c79995c873af5e29105db
Yevhenii Pozniak <jekamanu@gmail.com>
2025-04-03 15:18:54 +0300

    finished

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
-import { nanoid } from "nanoid";
 import * as fs from "node:fs/promises";
 import path from "node:path";
+import crypto from "node:crypto";
 
 const contactsPath = path.resolve("db", "contacts.json");
 console.log(contactsPath);
@@ -32,17 +32,94 @@ async function getContactById(contactId) {
   return contact;
 }
 
-getContactById("e6ywwRe4jcqxXfCZOj_1e").then((data) => console.log(data));
+// getContactById("e6ywwRe4jcqxXfCZOj_1e").then((data) => console.log(data));
 
 async function removeContact(contactId) {
   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+
+  const contacts = await readContacts();
+
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const deleteContact = contacts[index];
+
+  //* метод 1 без мутації
+  // const newContacts = [
+  //   ...contacts.slice(0, index),
+  //   ...contacts.slice(index + 1),
+  // ];
+
+  // await writeContacts(newContacts);
+
+  //* метод 2 з мутацією
+  contacts.splice(index, 1);
+
+  await writeContacts(contacts);
+
+  return deleteContact;
 }
 
-async function addContact(name, email, phone) {
+async function addContact(contact) {
+  //name, email, phone
   // ...твій код. Повертає об'єкт доданого контакту (з id).
   const contacts = await readContacts();
 
-  // const contact = { ...contacts, id: nanoid(), name, email, phone };
+  const newContact = { id: crypto.randomUUID(), ...contact };
+
+  contacts.push(newContact);
+
+  await writeContacts(contacts);
+
+  return newContact;
 }
 
-//продовження 1:36:13
+// addContact({
+//   name: "Adolf",
+//   email: "adolf@gmail.com",
+//   phone: "(007) 365333",
+// }).then((data) => console.log(data));
+
+async function updateContact(contactId, contactData) {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const updatedContact = { id: contactId, ...contactData };
+
+  //* варіант 1, без мутації
+  // const newContacts = [
+  //   ...contacts.slice(0, index),
+  //   updatedContact,
+  //   ...contacts.slice(index + 1),
+  // ];
+
+  // await writeContacts(newContact);
+
+  //* варіант 2, мутація
+  contacts[index] = updatedContact;
+
+  await writeContacts(contacts);
+
+  return updatedContact;
+}
+
+// update("e6ywwRe4jcqxXfCZOj_1e", {
+//   name: "Ninka",
+//   email: "ninka@gmail.com",
+//   phone: "(088) 32521231",
+// }).then((data) => console.log(data));
+
+export default {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { program } from "commander";
-import "./contacts.js";
+import Contacts from "./contacts.js";
 
 program
   .option("-a, --action <type>", "choose action")
@@ -17,18 +17,36 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       // ...
+      const contacts = await Contacts.listContacts();
+      console.table(contacts);
       break;
 
     case "get":
       // ... id
+      const contact = await Contacts.getContactById(id);
+      console.log(contact);
       break;
 
     case "add":
       // ... name email phone
+      const createdContact = await Contacts.addContact({ name, email, phone });
+      console.log(createdContact);
+      break;
+
+    case "update":
+      //id, name, email, phone
+      const updatedContact = await Contacts.getContactById(id, {
+        name,
+        email,
+        phone,
+      });
+      console.log(updatedContact);
       break;
 
     case "remove":
       // ... id
+      const removedContact = await Contacts.removeContact(id);
+      console.log(removedContact);
       break;
 
     default:
@@ -37,3 +55,4 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(options);
+// invokeAction(program.opts()).then(console.log);
